162c91c2ffa6044f12fcee50f6ac624a73e95f1f
Lorenzo Peve <lpeve01@gmail.com>
2023-08-30 10:45:23 -0500

    added random page

--- project_1/encyclopedia/urls.py
+++ project_1/encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("random/", views.random_page, name="random_entry"),
     path("<str:entry>", views.display_entry, name="entry"),
 ]
--- project_1/encyclopedia/views.py
+++ project_1/encyclopedia/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import Context, Template
+from django.shortcuts import redirect
 from markdown2 import Markdown
+import random
 
 markdowner = Markdown()
 
@@ -24,3 +25,20 @@ def display_entry(request, entry):
         return render(request, "encyclopedia/entry.html", {
             'entry': entry, 'content': html
         })
+    
+# def search_entry(request, entry):
+
+#     content = util.get_entry(entry)
+
+#     if content is None:
+#         return render(request, "encyclopedia/404.html", {'entry': entry})
+#     else:
+#         html = markdowner.convert(content)
+#         return render(request, "encyclopedia/entry.html", {
+#             'entry': entry, 'content': html
+#         })
+
+def random_page(request):
+    
+    entry = random.choice(util.list_entries())
+    return display_entry(request, entry)
