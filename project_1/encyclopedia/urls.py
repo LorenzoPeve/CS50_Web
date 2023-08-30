2a87d4d0125c45da1aa26d9a9b1634ffef183c28
Lorenzo Peve <lpeve01@gmail.com>
2023-08-30 14:02:12 -0500

    passing req#3

--- project_1/encyclopedia/urls.py
+++ project_1/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("random/", views.random_page, name="random_entry"),
+    path("query/", views.query, name="query"),
     path("<str:entry>", views.display_entry, name="entry"),
 ]
--- project_1/encyclopedia/views.py
+++ project_1/encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.shortcuts import redirect
+from django.urls import reverse
 from markdown2 import Markdown
 import random
 
@@ -15,9 +15,11 @@ def index(request):
     })
 
 def display_entry(request, entry):
+    """
+    Displays an encyclopedia entry if exists. Otherwise, renders error page.
+    """
 
     content = util.get_entry(entry)
-
     if content is None:
         return render(request, "encyclopedia/404.html", {'entry': entry})
     else:
@@ -25,20 +27,27 @@ def display_entry(request, entry):
         return render(request, "encyclopedia/entry.html", {
             'entry': entry, 'content': html
         })
-    
-# def search_entry(request, entry):
 
-#     content = util.get_entry(entry)
+def query(request):
+    """
+    Redirects the search query to the wiki page if it exists or renders a page
+    with "close" matches.
+    """
+    entries = util.list_entries()
+    post_data = request.POST
+    q = post_data.get('q')
 
-#     if content is None:
-#         return render(request, "encyclopedia/404.html", {'entry': entry})
-#     else:
-#         html = markdowner.convert(content)
-#         return render(request, "encyclopedia/entry.html", {
-#             'entry': entry, 'content': html
-#         })
+    if q in entries:
+        url = reverse('entry', args=[q])
+        return redirect(url)
+    else:
+        entries = [e for e in entries if q in e]
+        return render(request, "encyclopedia/search.html", {
+            'entry': q, 'entries': entries
+        })
+
 
 def random_page(request):
-    
+    """Displays a random encyclopedia entry."""
     entry = random.choice(util.list_entries())
     return display_entry(request, entry)
