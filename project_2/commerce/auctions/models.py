2aa77054ea6c6032a7af2d3d11e67520b1efa81f
Lorenzo Peve <lpeve01@gmail.com>
2023-09-05 08:43:05 -0500

    wip

--- project_2/commerce/auctions/models.py
+++ project_2/commerce/auctions/models.py
@@ -1,18 +1,30 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+CATEGORIES = [
+    "Electronics", "Fashion", "Home and Furniture", "Health and Beauty",
+    "Sports and Outdoors", "Toys and Games", "Books and Media", "Automotive"
+]
 
 class User(AbstractUser):
     pass
 
 class Listing(models.Model):
-    title = models.CharField(max_length=40, null=False)
+
+    cats = [(c,c) for c in CATEGORIES]
+    title = models.CharField(max_length=40, blank=False)
     description = models.CharField(max_length=255)
     current_bid =  models.FloatField()
-    image_url = models.URLField()
+    image_url = models.URLField(default=r"https://portfolio8aff35a6cba45be.blob.core.windows.net/djangoapp/image-not-available.jpg")
+    category = models.CharField(max_length=20, choices=cats)
+# class Bid(models.Model):
+#     pass
 
-class Bid(models.Model):
-    pass
+# class Comment(models.Model):
+#     pass
 
-class Comment(models.Model):
-    pass
+
+# class MyModel(models.Model):
+#     my_charfield = models.CharField(max_length=32, blank=False)
+
+# MyModel().save()
