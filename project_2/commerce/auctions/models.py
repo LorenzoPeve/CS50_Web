cd3dec34b47ff2d83e724ff31ace30e59bbc9445
Lorenzo Peve <lpeve01@gmail.com>
2023-09-05 23:02:03 -0500

    Update models.py

--- project_2/commerce/auctions/models.py
+++ project_2/commerce/auctions/models.py
@@ -2,8 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 CATEGORIES = [
-    "Electronics", "Fashion", "Home and Furniture", "Health and Beauty",
-    "Sports and Outdoors", "Toys and Games", "Books and Media", "Automotive"
+    "Electronics", "Fashion", "Home & Furniture", "Health & Beauty",
+    "Sports & Outdoors", "Toys & Games", "Books & Media", "Automotive",
+    'Collectibles & Art'
 ]
 
 class User(AbstractUser):
@@ -12,11 +13,24 @@ class User(AbstractUser):
 class Listing(models.Model):
 
     cats = [(c,c) for c in CATEGORIES]
+
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=40, blank=False)
     description = models.CharField(max_length=255)
     current_bid =  models.FloatField()
     image_url = models.URLField(default=r"https://portfolio8aff35a6cba45be.blob.core.windows.net/djangoapp/image-not-available.jpg")
     category = models.CharField(max_length=20, choices=cats)
+
+
+class Watchlist(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+
+
+
+
 # class Bid(models.Model):
 #     pass
 
