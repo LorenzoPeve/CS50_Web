357bc05a951cd595c0b0cd849d694362bbe80993
Lorenzo Peve <lpeve01@gmail.com>
2023-09-05 23:01:21 -0500

    Update urls.py

--- project_2/commerce/auctions/urls.py
+++ project_2/commerce/auctions/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("createlisting", views.create_listing, name="createlisting"),
+    path("shopcategory/<str:category>/", views.shop_by_category, name="shopcategory"),
+    path("shopcategory", views.shop_by_category, name="shopcategory"),
+    path("watchlist", views.add_to_watchlist, name="watchlist"),
 ]
