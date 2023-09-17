45e847d534a20e47d8cf8b7344ecc730a9c306dc
Lorenzo Peve <lpeve01@gmail.com>
2023-09-17 10:24:49 -0500

    mailbox added

--- project_3/mail/mail/static/mail/inbox.js
+++ project_3/mail/mail/static/mail/inbox.js
@@ -29,9 +29,8 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-
-  // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+  display_emails(mailbox)
 }
 
 function send_email() {
@@ -63,3 +62,59 @@ function send_email() {
     alert(error.message);
   });
 }
+
+function display_emails(mailbox) {
+  
+  get_emails_from_mailbox(mailbox).then(emails => {
+
+    const container = document.getElementById("emails-view");  
+    container.innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+    
+    emails.forEach(email => {
+
+      // Create card for email
+      const emailCard = document.createElement("div");
+      emailCard.className = "card w-75 shadow-0 border rounded-3 mb-3";
+
+      // Card header with sender and timestamp
+      const cardHeader = document.createElement("div");
+      cardHeader.className = "card-header mb-0 bg-secondary text-white d-flex justify-content-between";
+      cardHeader.innerHTML = `
+        <p class="mb-0"><small><strong>From:</strong> ${email.sender}</small></p>
+        <p class="mb-0"<small>${email.timestamp}</small></p>
+      `;
+
+      // Card Body
+      const cardBody = document.createElement("div");
+      cardBody.className = "card-body"
+      cardBody.innerHTML = `
+        <p class="card-title mb-0"><small><strong>Subject:</strong> ${email.subject}</small></p>
+        <p class="card-text mb-0"><small>${email.body.substring(0,100) + ' ...'}</small> </p>
+      `;
+
+      // Read vs Unread messages
+      if (email.read) {
+        cardBody.style.backgroundColor = "#cacfcd"
+      } else {
+        cardBody.style.backgroundColor = "white"
+      }
+    
+      emailCard.append(cardHeader)
+      emailCard.append(cardBody);
+      container.append(emailCard);
+    })
+  })
+}
+
+function get_emails_from_mailbox(mailbox) {
+
+  const url = `/emails/${mailbox}`;
+  emails = fetch(url)
+    .then(response => response.json());
+
+  return emails
+}
+
+
+
+
