7f1e7a64b34eff70c8f3549305924f065eb5755f
Lorenzo Peve <lpeve01@gmail.com>
2023-09-23 15:43:32 -0500

    Update network.js

--- project_4/network/network/static/network/network.js
+++ project_4/network/network/static/network/network.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     document.querySelectorAll('.like-button').forEach((button) => {
-        button.addEventListener('click', () => like_post(button.dataset.postid));
+        button.addEventListener('click', () => like_post(button));
     });
 
 });
 
+function like_post(button) {
 
-function like_post(post_id) {
-
+    let post_id = button.dataset.postid;
     fetch('http://127.0.0.1:8000/likeunlikepost', {
         method: 'POST',
         body: JSON.stringify({
@@ -16,5 +16,17 @@ function like_post(post_id) {
       })  
     })
     .then(response => response.json())
-    .then(result => console.log(result));
+    .then(result => {
+
+        let parentDiv = button.parentElement.parentElement;
+        let likeCounter = parentDiv.getElementsByClassName("like-counter")[0];
+        let current_count = Number(likeCounter.innerHTML);
+
+        if (result == 'like') {
+            likeCounter.innerHTML = current_count + 1;
+        }
+        else {
+            likeCounter.innerHTML = current_count - 1;
+        }
+    })
 };
